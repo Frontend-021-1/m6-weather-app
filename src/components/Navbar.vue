7f6f6bbc305ddc1b8bc881b360551102206c1f62
iVerdugoDev <ivan270@gmail.com>
2026-02-17 21:03:29 -0300

    modificar navbarbrand para que use routerlink

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,7 +10,7 @@ const busquedaFiltro = ref('')
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
-      <a class="navbar-brand" href="./index.html">El Tiempo</a>
+      <RouterLink class="navbar-brand" :to="{ name: 'Home' }">El Tiempo</RouterLink>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
