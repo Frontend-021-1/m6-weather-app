f0c20657a0f4a2360eb054386aa2c245c647cd0c
iVerdugoDev <ivan270@gmail.com>
2026-03-23 15:12:46 -0300

    agrega vista 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="text-center">
+    <h1>404 - Página no encontrada</h1>
+    <p>La página a la que intentas acceder no existe.</p>
+    <router-link :to="{ name: 'Home' }">Volver al inicio</router-link>
+  </div>
+</template>
+
+<style scoped></style>
